ecdbfb440a6d4a2d6f586d1d2b588316a4f1c7f5
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2023-12-27 10:29:57 +0000

    initial commit - keystatic blog + astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  output: "hybrid",
+});
