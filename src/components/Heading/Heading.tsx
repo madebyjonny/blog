c4cddbf13f72d2325c2e1a038577d2822e13ce50
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2023-12-27 15:00:07 +0000

    refactored heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,13 +1,8 @@
 import React, { type ComponentProps, type FC } from "react";
 import stylex from "@stylexjs/stylex";
 
-type HeadingProps = {
-  level: number;
-  children: React.ReactNode;
-};
-
 const styles = stylex.create({
-  highlighted: {
+  base: {
     color: "blue",
     ":hover": {
       opacity: "0.9",
@@ -15,8 +10,55 @@ const styles = stylex.create({
   },
 });
 
+const variants = stylex.create({
+  primary: {
+    color: "red",
+  },
+  secondary: {
+    color: "green",
+  },
+});
+
+const types = stylex.create({
+  h1: {
+    color: "red",
+  },
+  h2: {
+    color: "green",
+  },
+  h3: {
+    color: "blue",
+  },
+  h4: {
+    color: "yellow",
+  },
+  h5: {
+    color: "pink",
+  },
+  h6: {
+    color: "black",
+  },
+});
+
+type HeadingProps = {
+  type: keyof typeof types;
+  children: React.ReactNode;
+  variant: keyof typeof variants;
+};
+
 export const Heading: FC<ComponentProps<"h1"> & HeadingProps> = (props) => {
-  const { level, children } = props;
-  const HLevel = `h${level}` as keyof JSX.IntrinsicElements;
-  return <HLevel {...stylex.props([styles.highlighted])}>{children}</HLevel>;
+  const { type = "h1", children, variant } = props;
+  const Htype = type;
+
+  return (
+    <Htype
+      {...stylex.props(
+        styles.base,
+        type && types[Htype],
+        variant && variants[variant],
+      )}
+    >
+      {children}
+    </Htype>
+  );
 };
