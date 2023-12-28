b3dab0f4a5af0d915dd91a03f1ae89e5073ba80b
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2023-12-28 21:25:02 +0000

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    markdoc(),
-    ...(process.env.PRODUCTION ? [] : [keystatic()]),
-  ],
+  integrations: [react(), markdoc(), ...(process.env.PRODUCTION ? [] : [keystatic()])],
   output: "hybrid",
+  adapter: vercel()
 });
