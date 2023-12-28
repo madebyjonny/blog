42a204fceae2451c078a0f693dfe7db52561b64b
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2023-12-28 21:35:39 +0000

    updated article and changed keystatic url security

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     react(),
     markdoc(),
-    ...(!process.env.PRODUCTION ? [] : [keystatic()]),
+    ...(process.env.PRODUCTION ? [] : [keystatic()]),
   ],
   output: "hybrid",
   adapter: vercel(),
