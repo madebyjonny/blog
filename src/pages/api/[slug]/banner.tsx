180e58713c8ac007773d4378deb676e18ce61cf4
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2023-12-29 23:16:02 +0000

    adjusted the social card

--- src/pages/api/[slug]/banner.tsx
+++ src/pages/api/[slug]/banner.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
+export const Image = (imageURL: string) =>
+  React.createElement("img", {
+    style: {
+      width: "70px",
+    },
+    src: imageURL,
+  });
+
+export const BrandingText = (text: string) =>
+  React.createElement(
+    "span",
+    {
+      style: {
+        color: "#000",
+        textTransform: "uppercase",
+        letterSpacing: "0.5px",
+      },
+    },
+    text,
+  );
+
+export const Branding = () =>
+  React.createElement(
+    "span",
+    {
+      style: {
+        display: "flex",
+        gap: "16px",
+        fontSize: "2rem",
+        textTransform: "uppercase",
+        fontFamily: "sans-serif",
+        margin: "0 0 24px 0",
+        color: "#000",
+      },
+    },
+    Image("https://blog.madebyjonny.com/logo.svg"),
+    BrandingText("madebyjonny"),
+  );
+
 export const Heading = (text: string) =>
   React.createElement(
     "h1",
@@ -30,23 +69,39 @@ export const Paragraph = (text: string) =>
     text,
   );
 
-export const Banner = (title: string, summary: string, category?: string) => {
-  const titleComp = Heading(title);
-  const pComp = Paragraph(summary);
+export const Container = (
+  title: string,
+  summary: string,
+  category?: string,
+) => {
   return React.createElement(
     "div",
     {
       style: {
-        padding: "32px",
+        display: "flex",
+        flexDirection: "column",
+      },
+    },
+    Heading(title),
+    Paragraph(summary),
+  );
+};
+
+export const Banner = (title: string, summary: string, category?: string) => {
+  return React.createElement(
+    "div",
+    {
+      style: {
+        padding: "64px",
         width: "100%",
         height: "100%",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
-        background: "pink",
+        justifyContent: "space-between",
+        background: "#fff",
       },
     },
-    titleComp,
-    pComp,
+    Branding(),
+    Container(title, summary),
   );
 };
